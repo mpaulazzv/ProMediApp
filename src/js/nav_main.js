98c5555d09519b67a219ae9aed6c90ba84a7a356
Juan Volkmar <juan.volkmar@upb.edu.co>
2024-08-25 11:13:07 -0500

    Issue: Imágenes del menú

--- src/js/nav_main.js
+++ src/js/nav_main.js
@@ -8,19 +8,19 @@ class navMain extends HTMLElement{
         <div class="mainNav">
             <a href="" class="mainOpc">
                 <div id="btn_semestres">
-                    <img src="/src/assets/semestre.svg" alt="">
+                    <img src='http://127.0.0.1:3000/src/assets/semestre.svg' alt="">
                     <p>Semestres</p>
                 </div>
             </a>
             <a href="" class="mainOpc">
                 <div id="btn_Home" class="">
-                    <img src="/src/assets/home.svg" alt="" id="img-home">
+                    <img src="http://127.0.0.1:3000/src/assets/home.svg" alt="" id="img-home">
                     <p>Home</p>
                 </div>
             </a>
             <a href="" class="mainOpc">
                 <div id="btn_perfil">
-                    <img src="/src/assets/user.svg" alt="">
+                    <img src="http://127.0.0.1:3000/src/assets/user.svg" alt="">
                     <p>Perfil</p>
                 </div>
             </a>
